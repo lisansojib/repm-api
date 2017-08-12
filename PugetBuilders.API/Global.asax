768dbc221f6875c31b8248505f03eb0365faf847
lisansojib <lisancseru@gmail.com>
2017-08-12 22:59:20 +0600

    Initial commit

---
+++ PugetBuilders.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PugetBuilders.API.WebApiApplication" Language="C#" %>
---
+++ PugetBuilders.API/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace PugetBuilders.API.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            var user = Context.User.Identity.Name;
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
